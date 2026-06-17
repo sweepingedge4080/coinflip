60220b58d3923ece91efacd78cb634c583205b6d
sweepingedge4080 <yujigraphics@gmail.com>
2026-06-17 20:41:43 +0300

    Update User.js

--- models/User.js
+++ models/User.js
@@ -10,18 +10,22 @@ const UserSchema = new mongoose.Schema({
     losses: { type: Number, default: 0 },
     bestStreak: { type: Number, default: 0 },
     currentStreak: { type: Number, default: 0 },
+    level: { type: Number, default: 1 },
+    xp: { type: Number, default: 0 },
     isAdmin: { type: Boolean, default: false },
     pendingDeposits: [{ type: mongoose.Schema.Types.ObjectId, ref: 'DepositRequest' }],
     pendingWithdrawals: [{ type: mongoose.Schema.Types.ObjectId, ref: 'WithdrawRequest' }],
     createdAt: { type: Date, default: Date.now }
 });
 
+// Hash password before saving
 UserSchema.pre('save', async function(next) {
     if (!this.isModified('password')) return next();
     this.password = await bcrypt.hash(this.password, 10);
     next();
 });
 
+// Compare password method
 UserSchema.methods.comparePassword = async function(password) {
     return await bcrypt.compare(password, this.password);
 };
